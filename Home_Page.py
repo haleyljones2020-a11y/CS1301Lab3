a956b93d17dce7b30eb705f36893c8fd3a4fe38b
haleyljones2020-a11y <haleyljones2020@gmail.com>
2026-07-27 12:40:43 -0400

    Revise title and add plantExplorer description
    Updated title and team member details in Home_Page.py. Added a description for the plantExplorer page.

--- Home_Page.py
+++ Home_Page.py
@@ -1,19 +1,19 @@
 import streamlit as st
 
 # Title of App
-st.title("Web Development Lab03")
+st.title("Web Development Lab03: What's that plant?!")
 
 # Assignment Data 
 # TODO: Fill out your team number, section, and team members
 
 st.header("CS 1301")
-st.subheader("Team XX, Web Development - Section X")
-st.subheader("Team Member 1, Team Member 2")
+st.subheader("Team Plant Experts, Web Development - 1301-A")
+st.subheader("Haley Jones, Lauren Black")
 
 
 # Introduction
 # TODO: Write a quick description for all of your pages in this lab below, in the form:
-#       1. **Page Name**: Description
+#       1. **plantExplorer**: This page allows you to learn more about any plant you may have an interest in. Simply create a Trefle account, insert your API token, and provide a plant name and the code will take care of the rest! Learn more about the family, names, genus, and family distribution of any plant!
 #       2. **Page Name**: Description
 #       3. **Page Name**: Description
 #       4. **Page Name**: Description
@@ -21,10 +21,9 @@ st.subheader("Team Member 1, Team Member 2")
 st.write("""
 Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
 
-1.
+1. **plantExplorer**: This page allows you to learn more about any plant you may have an interest in. Simply create a Trefle account, insert your API token, and provide a plant name and the code will take care of the rest! Learn more about the family, names, genus, and family distribution of any plant!
 2.
 3.
 4.
 
 """)
-
